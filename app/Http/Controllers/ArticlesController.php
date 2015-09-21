fe8d346bf5629eb45370da4a1f01c19a551c0f76
Kevin Elberger <kevelberger@gmail.com>
2015-09-20 19:26:32 -0700

    include edit functionality and add form partial for create/edit

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -4,10 +4,19 @@ namespace App\Http\Controllers;
 
 use App\Article;
 use App\Http\Requests;
+use Illuminate\Http\Request;
+use Illuminate\Http\HttpResponse;
+use App\Http\Requests\ArticleRequest;
 use App\Http\Controllers\Controller;
 
 class ArticlesController extends Controller
 {
+
+    /**
+     * Display all articles.
+     *
+     * @return \Illuminate\View\View
+     */
     public function index() {
 
         // Fix this later
@@ -18,6 +27,12 @@ class ArticlesController extends Controller
         return view('articles.index', compact('articles'));
     }
 
+    /**
+     * Display a specific article.
+     *
+     * @param $id
+     * @return \Illuminate\View\View
+     */
     public function show($id) {
 
         $article = Article::findOrFail($id);
@@ -25,6 +40,11 @@ class ArticlesController extends Controller
         return view('articles.show', compact('article'));
     }
 
+    /**
+     * Create an article.
+     *
+     * @return \Illuminate\View\View
+     */
     public function create() {
 
         return view('articles.create');
@@ -37,15 +57,39 @@ class ArticlesController extends Controller
      * @param Requests\CreateArticleRequest $request
      * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
      */
-    public function store(Requests\CreateArticleRequest $request) {
+    public function store(ArticleRequest $request) {
 
         Article::create($request->all());
 
         return redirect('articles');
     }
 
-    public function edit() {
+    /**
+     *  Edits an article.
+     *
+     * @param $id
+     * @return \Illuminate\View\View
+     */
+    public function edit($id) {
 
-        return view('articles.edit');
+        $article = Article::findOrFail($id);
+
+        return view('articles.edit', compact('article'));
+    }
+
+    /**
+     * Update an existing article.
+     *
+     * @param $id
+     * @param ArticleRequest $request
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     */
+    public function update($id, ArticleRequest $request) {
+
+        $article = Article::findOrFail($id);
+
+        $article->update($request->all());
+
+        return redirect('articles');
     }
 }
--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -4,33 +4,8 @@
     <h1>Create an Article</h1>
 
     {!! Form::open(['url' => 'articles']) !!}
-        <div class="form-group">
-            {!! Form::label('title', 'Title:') !!}
-            {!! Form::text('title', null, ['class' => 'form-control']) !!}
-        </div>
-
-        <div class="form-group">
-            {!! Form::label('body', 'Body:') !!}
-            {!! Form::textarea('body', null, ['class' => 'form-control']) !!}
-        </div>
-
-        <div class="form-group">
-            {!! Form::label('published_at', 'Publish On:') !!}
-            {!! Form::input('date', 'published_at', date('Y-m-d'), ['class' => 'form-control']) !!}
-        </div>
-
-        <div class="form-group">
-            {!! Form::submit('Create Article', ['class' => 'btn btn-primary form-control']) !!}
-        </div>
+        @include ('articles.form', ['submitButtonText' => 'Add Article'])
     {!! Form::close() !!}
 
-    @if (count($errors) > 0)
-        <div class="alert alert-danger">
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
+    @include ('errors.list')
 @stop
